9b77093e59044b5d27a6f2414de8749b505e1632
dangen-effy <dangen.effy@gmail.com>
2019-04-18 16:42:05 +0900

    .

--- server/index.js
+++ server/index.js
@@ -15,8 +15,7 @@ const pool = mysql.createPool({
 const promisePool = pool.promise()
 
 const SENDGRID_URL = 'https://api.sendgrid.com/v3/contactdb/recipients'
-const SENDGRID_API_KEY =
-  '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'
+const SENDGRID_API_KEY = ''
 
 function validateEmail(email) {
   const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
