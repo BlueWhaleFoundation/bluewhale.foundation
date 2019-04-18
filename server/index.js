f9a8c5354f7eac820853fb2094709060b98114be
dangen-effy <dangen.effy@gmail.com>
2019-04-18 11:43:21 +0900

    .

--- server/index.js
+++ server/index.js
@@ -7,6 +7,11 @@ const SENDGRID_URL = 'https://api.sendgrid.com/v3/contactdb/recipients'
 const SENDGRID_API_KEY =
   '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'
 
+function validateEmail(email) {
+  const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  return re.test(email)
+}
+
 const app = express()
 
 app.use(bodyParser.json())
@@ -18,17 +23,24 @@ app.use(
 app.post('/subscribe', async function(req, res) {
   console.log('구독 요청!', req.body)
   try {
+    if (!validateEmail(req.body[0].email)) {
+      throw new Error('이메일 양식이 틀렸습니다')
+    }
+
     const result = await axios.post(SENDGRID_URL, req.body, {
       headers: { Authorization: 'Bearer ' + SENDGRID_API_KEY },
     })
 
     res.send({ result: 'sucess' })
   } catch (err) {
+    console.log(err)
     res.sendStatus(400)
   }
 })
 
 app.delete('/subscribe', async function(req, res) {
+  console.log('구독 취소 요청!', req.body)
+
   try {
     const result = await axios.delete(SENDGRID_URL, {
       data: req.body,
